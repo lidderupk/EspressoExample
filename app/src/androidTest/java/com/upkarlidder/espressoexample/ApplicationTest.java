6713f3f608aff93349891679506cc5a485d7bfe3
Upkar Lidder <lidderupk@gmail.com>
2016-08-19 10:57:36 -0700

    set up espresso and added the first test

--- app/src/androidTest/java/com/upkarlidder/espressoexample/ApplicationTest.java
+++ app/src/androidTest/java/com/upkarlidder/espressoexample/ApplicationTest.java
@@ -1,16 +1,45 @@
 package com.upkarlidder.espressoexample;
 
 import android.app.Application;
+import android.support.test.filters.LargeTest;
+import android.support.test.filters.SmallTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Test
+    public void testingeverything(){
+        assertEquals(true, true);
+    }
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+
+
 
 
 }
